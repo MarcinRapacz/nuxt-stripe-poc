7907ce316dc25c91c18184a467ea267028cea7fe
m.rapacz <m.rapacz@getprintbox.com>
2024-09-05 21:39:20 +0200

    c

--- pages/payment-element.vue
+++ pages/payment-element.vue
@@ -1,15 +1,29 @@
 <script lang="ts" setup>
 const { data } = await useFetch("/stripe", { method: "post" });
 
-onMounted(() => {
-  const stripe = useNuxtApp().$stripe;
+const stripe = useNuxtApp().$stripe;
 
-  if (!stripe || !data.value?.clientSecret) throw new Error();
-  const elements = stripe.elements({
-    appearance: { theme: "stripe" },
-    clientSecret: data.value.clientSecret,
+if (!stripe || !data.value?.clientSecret) throw new Error();
+const elements = stripe.elements({
+  appearance: { theme: "stripe" },
+  clientSecret: data.value.clientSecret,
+});
+
+async function handleSubmit(e: any) {
+  e.preventDefault();
+  if (!stripe) throw new Error();
+
+  const { error } = await stripe.confirmPayment({
+    elements,
+    confirmParams: {
+      return_url: "http://localhost:3000/success",
+    },
   });
 
+  console.log(error);
+}
+
+onMounted(() => {
   const paymentElementOptions = {
     layout: "tabs",
   };
@@ -22,4 +36,5 @@ onMounted(() => {
 <template>
   <h1>Payment Element</h1>
   <div id="payment-element" />
+  <button @click="handleSubmit">Submit</button>
 </template>
